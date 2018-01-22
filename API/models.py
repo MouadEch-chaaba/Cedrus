ed7d904decbd60d6daa5779e9e360f3c8619b1aa
Mouad <mouad.echchaaba@gmail.com>
2018-01-22 20:44:03 +0000

    Creating the User Model Class

--- API/models.py
+++ API/models.py
@@ -1,6 +1,7 @@
 from cgi import maxlen
 
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
@@ -9,3 +10,10 @@ class Idea(models.Model):
     idea_text = models.TextField()
     creation_date = models.DateTimeField(auto_now=True)
     modification_date = models.DateTimeField(default=None)
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    email = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=100)
