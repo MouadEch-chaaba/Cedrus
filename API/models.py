e06adca773205eb11ce9fecb605a2dad6c3dbfae
Mouad <mouad.echchaaba@gmail.com>
2018-01-22 20:30:39 +0000

    Creating the Idea Model Class

--- API/models.py
+++ API/models.py
@@ -1,3 +1,11 @@
+from cgi import maxlen
+
 from django.db import models
 
+
 # Create your models here.
+
+class Idea(models.Model):
+    idea_text = models.TextField()
+    creation_date = models.DateTimeField(auto_now=True)
+    modification_date = models.DateTimeField(default=None)
